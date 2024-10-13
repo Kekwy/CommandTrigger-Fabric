051bfae88ca05d97d7b321728679917f555423cb
Kekwy <keekkewy@qq.com>
2024-10-13 19:17:19 +0800

    feat: 基本功能已完成

--- src/client/java/com/kekwy/nmo/cmdtgr/client/MainClient.java
+++ src/client/java/com/kekwy/nmo/cmdtgr/client/MainClient.java
@@ -5,6 +5,8 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+import java.util.Arrays;
+
 public class MainClient implements ClientModInitializer {
 
     private static final Identifier CHANNEL_ID = new Identifier("cmdtgr", "channel");
@@ -14,13 +16,13 @@ public class MainClient implements ClientModInitializer {
         // 注册客户端接收来自服务器的消息
         ClientPlayNetworking.registerGlobalReceiver(CHANNEL_ID, (client, handler, buf, responseSender) -> {
             // 从 buf 读取服务器发送的数据
-            String receivedMessage = buf.readString(32767);
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.readBytes(bytes);
 
             // 在客户端主线程中处理数据
             client.execute(() -> {
                 // 执行客户端的操作，比如显示聊天消息
-                client.player.sendMessage(Text.of("Server says: " + receivedMessage), false);
-
+                client.player.networkHandler.sendCommand(new String(bytes));
                 // 你也可以在这里编写自定义逻辑来响应该请求
             });
         });
